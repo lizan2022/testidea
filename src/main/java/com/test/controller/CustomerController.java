24bf961513737e2f3ca6282624ff1321f8f1af48
lizan2022 <120350398+lizan2022@users.noreply.github.com>
2022-12-13 20:23:45 +0800

    Update CustomerController.java

--- src/main/java/com/test/controller/CustomerController.java
+++ src/main/java/com/test/controller/CustomerController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
 
-  public String yestname(){
+  public String testname(){
   }
 }
 
